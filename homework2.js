71a62a1ac75c9735c77d86cfacb8cb6335b6a1e3
JacquelinCampa <jacquelin_valdez@yahoo.com>
2025-06-19 00:55:53 -0500

    Update homework2.js

--- homework2.js
+++ homework2.js
@@ -10,6 +10,7 @@ Description: homework 2 JavaScript to Redisplay/validate patient data from a for
 // JS for dynamic date 
 document.getElementById("today").innerHTML = new Date().toLocaleDateString();
 
+// Displays all form data in a new section 
 function reviewForm() {
   alert("Review Form clicked!");
 }
@@ -21,47 +22,6 @@ function reviewForm() {
 }
 */
 
-// Displays all form data in a new section 
-/* function reviewForm() {
-  var formcontents = document.getElementById("signup");
-  var formoutput;
-  var datatype;
-  var i;
-  formoutput = "<table class='output'><th>Dataname</th><th>Type</th><th>Value</th>";
-  for (i = 0; i < formcontents.length; i++) {
-    console.log("item: "+i+" "+formcontents.elements[i].name+" = "+formcontents.elements[i].value);
-    if (formcontents.elements[i].value !="") {
-      datatype = formcontents.elements[i].type;
-      switch (datatype) {
-        case "checkbox":
-          if (formcontents.elements[i].checked) {
-            formoutput = formoutput + "<tr><td align='right'>"+formcontents.elements[i].name+"</td>";
-            formoutput = formoutput +"<td align='right'>"+ datatype + "</td>";
-            formoutput = formoutput +"<td class='outputdata'>Checked</td></tr>";
-          }
-          break;
-        case "radio":
-          if (formcontents.elements[i].checked) {
-            formoutput = formoutput + "<tr><td align='right'>"+formcontents.elements[i].name+"</td>";
-            formoutput = formoutput +"<td align='right'>"+ datatype + "</td>";
-            formoutput = formoutput +"<td class='outputdata'>"+ formcontents.elements[i].value+"</td></tr>";
-          }
-          break;
-        case "button": case "submit": case "reset":
-          break;
-        default:
-          formoutput = formoutput + "<tr><td align='right'>"+formcontents.elements[i].name+"</td>";
-          formoutput = formoutput +"<td align='right'>"+ datatype + "</td>";
-          formoutput = formoutput +"<td class='outputdata'>"+ formcontents.elements[i].value+"</td></tr>";
-      }
-    }
-  }
-   if (formoutput.length>0) { 
-      formoutput = formoutput + "</table>";
-      document.getElementById("outputformdata").innerHTML = formoutput;
-   }
-}
-*/
 
 // Displays the range slider value 
 var slider = document.getElementById("comfortrange");
