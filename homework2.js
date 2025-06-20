412a608c4339627f1759f49eb1d150699caffdb4
JacquelinCampa <jacquelin_valdez@yahoo.com>
2025-06-19 19:04:51 -0500

    Update homework2.js

--- homework2.js
+++ homework2.js
@@ -325,18 +325,15 @@ function validatePhone() {
 
   if (digits === '') {
     document.getElementById("phone_message").innerHTML = "Phone number cannot be blank.";
-    error_flag = 1;
-    return; 
+    error_flag = 1; 
   }
   if (digits.length !== 10) {
     document.getElementById("phone_message").innerHTML = "Invalid. Enter a 10 digit phone number.";
     error_flag = 1;
-    return;
   }
   if (digits.charAt(0) === '1') {
     document.getElementById("phone_message").innerHTML = "Phone number cannot start with 1.";
     error_flag = 1;
-    return;
   }
   phone.value = digits.replace(/(\d{3})(\d{3})(\d{4})/, '$1-$2-$3');
   document.getElementById("phone_message").innerHTML = ""; 
