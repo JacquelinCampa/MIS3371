e904627fb9344bd97011686148327443ec92820c
JacquelinCampa <jacquelin_valdez@yahoo.com>
2025-06-23 18:39:51 -0500

    Update homework1.js

--- homework1.js
+++ homework1.js
@@ -1,1 +1,8 @@
-
+/*
+Author: Jacquelin Valdez Campa
+Program name: homework1.js
+Date created: 06/02/25
+Date last edited: 06/12/25
+Version: 1.0
+Description: Homework 1 JavaScript for date field
+*/
